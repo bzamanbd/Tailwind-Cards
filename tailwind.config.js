7932677b896c0d65bab7aeb4db69712296dcbd1c
bzamanbd <riponzamanbd@gmail.com>
2024-09-04 15:15:47 +0600

    Done: Elegant-Card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    
+    extend: {
+      fontFamily: {
+        sand: ['Quicksand', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
